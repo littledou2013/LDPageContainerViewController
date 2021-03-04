94c4db3bee6224a0f31686e838cf646cd0190b84
陈小双 <chenxiaoshuang1@corp.netease.com>
2021-03-04 18:46:24 +0800

    接口公开

--- Sources/LDPageViewController/LDPageContainerViewController.swift
+++ Sources/LDPageViewController/LDPageContainerViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class LDPageContainerViewController: UIViewController, UIScrollViewDelegate {
+public class LDPageContainerViewController: UIViewController, UIScrollViewDelegate {
     
     weak var dataSource: LDPageContainerViewControllerDataSource?
     
