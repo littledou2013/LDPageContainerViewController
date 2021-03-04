33e481687b24edfb116d81d206615e0143fa3039
陈小双 <chenxiaoshuang1@corp.netease.com>
2021-03-04 21:02:06 +0800

    支持OC

--- Package.swift
+++ Package.swift
@@ -20,7 +20,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LDPageViewController",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources",
+            sources: ["LDPageViewController"],
+            cSettings: [
+                .headerSearchPath("LDPageViewController")
+            ]
+        ),
         .testTarget(
             name: "LDPageViewControllerTests",
             dependencies: ["LDPageViewController"]),
